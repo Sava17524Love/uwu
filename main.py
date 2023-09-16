9598d74b6e7dd6ef7511bb7bdbe0773d5dad4e17
Sava17524Love <144526618+Sava17524Love@users.noreply.github.com>
2023-09-16 13:18:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from random import randint
+app = QApplication([])
+my_win = QWidget()
+my_win.setWindowTitle("Визначник переможця")
+my_win.move(100, 100)
+my_win.resize(400, 200)
+button = QPushButton(my_win)
+button.setText('Згенерувати')
+button.move(140, 130)
+
+text = QLabel(my_win)
+text.setText('Натисніть, щоб дшзнатись переможця')
+text.move(70, 10)
+
+winner = QLabel(my_win)
+winner.setText('?')
+winner.move(190, 70)
+
+def show_winner():
+    number = randint(1, 100)
+    winner.setText(str(number))
+    text.setText('Переможець:')
+    text.move(190, 10)
+
+button.clicked.connect(show_winner)
+
+my_win.show()
+app.exec_()
